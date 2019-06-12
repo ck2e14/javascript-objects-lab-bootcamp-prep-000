f759699c5f566d04a21538bd5c5f077000b743ca
Christopher Kennedy <chris94kennedy@gmail.com>
2019-06-12 14:15:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {prop: '1'}
 
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, value)
+  return Object.assign({}, obj, {[key]:value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
